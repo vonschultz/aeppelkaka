8fb51938f8569b1fa091434d6c3e2b54d7ccf2b4
veronika-vonschultz <veronica.vonschultz@hotmail.com>
2024-09-15 13:21:10 +0100

    Update formatting

--- src/inputtypeconversion.js
+++ src/inputtypeconversion.js
@@ -1,10 +1,10 @@
 export function convertFakePasswordInput () {
-	const collection = document.getElementsByClassName("fake_password_input");
+  const collection = document.getElementsByClassName('fake_password_input')
 
-	for (let i = 0; i < collection.length; i++) {
-		var input = collection[i];
-		input.type = "text";
-	}
+  for (let i = 0; i < collection.length; i++) {
+    const input = collection[i]
+    input.type = 'text'
+  }
 
-	console.log('Updated fake password input fields to text.')
+  console.log('Updated fake password input fields to text.')
 }
